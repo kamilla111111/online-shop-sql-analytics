98d578b4dc602833564536e50bfb0b0ed419adc3
kamilla111111 <k_kamalova@internet.ru>
2026-06-14 18:12:49 +0300

    shema.sql

--- shema.sql
+++ shema.sql
@@ -1,37 +1,42 @@
 DROP DATABASE IF EXISTS online_shop;
 CREATE DATABASE online_shop;
-USE online_shop
+USE online_shop;
 
 CREATE TABLE customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    city VARCHAR(50),
-    created_at DATE DEFAULT CURRENT_DATE
+    customer_id INT AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    city VARCHAR(50),
+    created_at DATE,
+    PRIMARY KEY (customer_id),
+    UNIQUE (email)
 );
 
 CREATE TABLE products (
-    product_id INT PRIMARY KEY AUTO_INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
-    category VARCHAR(50),
-    price DECIMAL(10,2) NOT NULL,
-    stock INT DEFAULT 0
+    product_id INT AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    price DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT 0,
+    PRIMARY KEY (product_id)
 );
 
 CREATE TABLE orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT NOT NULL,
-    order_date DATE DEFAULT CURRENT_DATE,
-    status VARCHAR(30) DEFAULT 'completed',
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    order_id INT AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    status VARCHAR(30) DEFAULT 'completed',
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE order_items (
-    item_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    unit_price DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    item_id INT AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
